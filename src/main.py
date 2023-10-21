6b2ab5182cf6d83774cff7beb4f4e23b29b2c770
Ali EL AMRI <contact@alielamri.com>
2023-10-21 11:22:51 +0300

    add logging

--- src/main.py
+++ src/main.py
@@ -11,10 +11,19 @@ load_dotenv()
 unreal_store_url = "https://www.unrealengine.com/marketplace/en-US/assets?tag=4910"
 thumbnail_url = "https://cdn-icons-png.flaticon.com/512/4712/4712109.png"
 
+# Logging
+log_file = os.path.join(os.path.dirname(__file__), "../", "output.log")
+
+
+def write_log(message):
+    with open(log_file, "a") as f:
+        f.write(f"{message}\n")
+
 
 # Scrape UE's store page and fetch free of the month asset data
 # These include: title, author, and original price
 def scrape(url):
+    write_log(f"Scraping UE's store page {url}")
     response = requests.get(url)
     page = BeautifulSoup(response.content, "html.parser")
     free_assets = page.find_all("div", class_="asset-container catalog asset-full")
@@ -33,11 +42,13 @@ def scrape(url):
             {"title": title, "author": author, "original_price": original_price}
         )
 
+    write_log(f"Found {len(asset_data)} free assets")
     return asset_data
 
 
 # Sends a webhook POST request to Discord
 def send_webhook(url, data):
+    write_log(f"Sending webhook {url}")
     description = ""
 
     for asset in data:
@@ -66,6 +77,8 @@ def send_webhook(url, data):
         },
     )
 
+    write_log("Webhook sent")
+
 
 # The job to be executed on a schedule
 def run():
@@ -73,6 +86,10 @@ def run():
     if time.strftime("%d") > 7:
         return
 
+    with open(log_file, "w") as f:
+        pass
+
+    write_log(time.strftime("%Y-%m-%d %H:%M:%S"))
     asset_data = scrape(unreal_store_url)
     send_webhook(os.getenv("DISCORD_WEBHOOK_URL"), asset_data)
 
@@ -85,3 +102,7 @@ def main():
         schedule.run_pending()
         time.sleep(1)
     run()
+
+
+if __name__ == "__main__":
+    main()
